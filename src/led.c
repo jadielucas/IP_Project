3c455ad8911cc1b09ac542dc3f4ec21e1ca22c52
WessTinho <pedrolucasdiogenes23@gmail.com>
2025-02-17 11:30:29 -0300

    Document src folder

--- src/led.c
+++ src/led.c
@@ -1,31 +1,47 @@
 #include "inc/led.h"
 
+// Structure to store a repeating timer
 struct repeating_timer timer;
 
-// Function to setup GPIO pins
+/**
+ * @brief Configures GPIO pins for the LEDs.
+ * 
+ * Initializes and sets the direction of GPIO pins used for the red and green LEDs.
+ */
 void setup_leds()
 {
-    gpio_init(RED_LED);
-    gpio_init(GREEN_LED);
-    gpio_set_dir(RED_LED, GPIO_OUT);
-    gpio_set_dir(GREEN_LED, GPIO_OUT);
+    gpio_init(RED_LED);   // Initialize the red LED pin
+    gpio_init(GREEN_LED); // Initialize the green LED pin
+    gpio_set_dir(RED_LED, GPIO_OUT);   // Set the red LED as output
+    gpio_set_dir(GREEN_LED, GPIO_OUT); // Set the green LED as output
 }
 
+/**
+ * @brief Updates the LED status based on the dB level detected by the microphone.
+ *
+ * @param t Pointer to the repeating timer structure.
+ * @return true Always returns true to keep the timer running.
+ *
+ * The function checks the dB level contained in the micdata_t structure and controls
+ * the LEDs based on a predefined threshold (DB_THRESHOLD). If the dB level exceeds the threshold,
+ * the red LED turns on and the green LED turns off; otherwise, the green LED turns on and the red LED turns off.
+ */
 bool update_led_status(struct repeating_timer *t)
 {
-
+    // Cast the timer user data to the micdata_t structure
     micdata_t *micdata = (micdata_t *)t->user_data;
 
+    // Check if the dB level exceeds the defined threshold
     if (micdata->dB > DB_THRESHOLD)
     {
-        gpio_put(RED_LED, 1);
-        gpio_put(GREEN_LED, 0);
-        return true;
+        gpio_put(RED_LED, 1);  // Turn on the red LED
+        gpio_put(GREEN_LED, 0); // Turn off the green LED
     }
     else
     {
-        gpio_put(RED_LED, 0);
-        gpio_put(GREEN_LED, 1);
-        return true;
+        gpio_put(RED_LED, 0);  // Turn off the red LED
+        gpio_put(GREEN_LED, 1); // Turn on the green LED
     }
+    
+    return true; // Return true to keep the timer running continuously
 }
--- src/wifi.c
+++ src/wifi.c
@@ -1,5 +1,14 @@
 #include "inc/wifi.h"
 
+/**
+ * @brief Initializes the Wi-Fi connection.
+ *
+ * This function initializes the Wi-Fi module and attempts to connect to a Wi-Fi network.
+ * If initialization or connection fails, it prints an error message and returns 1.
+ * If the connection is successful, it prints a confirmation message.
+ *
+ * @return int Returns 0 on success, 1 on failure.
+ */
 int wifi_init()
 {
     if (cyw43_arch_init())
@@ -9,6 +18,7 @@ int wifi_init()
     }
     cyw43_arch_enable_sta_mode();
     printf("Conectando ao Wi-Fi...\n");
+    
     if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 8000))
     {
         printf("Falha ao conectar ao Wi-Fi\n");
