664fc1eca0ef2bccc0289fc0b379330316c085a7
WessTinho <pedrolucasdiogenes23@gmail.com>
2025-02-23 20:51:52 -0300

    Documentation of the files in the inc folder

--- inc/led.h
+++ inc/led.h
@@ -1,11 +1,28 @@
 #ifndef LED_H
 #define LED_H
 
-#include "pico/stdlib.h"
+#include "pico/stdlib.h"  // Standard library for Raspberry Pi Pico
 
+// External declaration of a repeating timer used for LED control
 extern struct repeating_timer timer;
 
+/**
+ * @brief Initializes the LEDs.
+ *
+ * This function sets up the GPIO pins and configurations required 
+ * to control the LEDs.
+ */
 void setup_leds();
+
+/**
+ * @brief Callback function to update the LED status.
+ *
+ * This function is triggered by the repeating timer to change the LED state,
+ * allowing for effects like blinking or status indication.
+ *
+ * @param t Pointer to the repeating_timer structure triggering the callback.
+ * @return true if the timer should continue running, false to stop.
+ */
 bool update_led_status(struct repeating_timer *t);
 
 #endif
--- inc/mic.h
+++ inc/mic.h
@@ -1,20 +1,47 @@
 #ifndef MIC_H
 #define MIC_H
 
-#define SAMPLE_COUNT 100
-#define DB_THRESHOLD 36
+#define SAMPLE_COUNT 100   // Number of samples to collect from the microphone
+#define DB_THRESHOLD 36    // Decibel threshold for signal processing or triggering events
 
-#include "pico/stdlib.h"
+#include "pico/stdlib.h"   // Standard library for Raspberry Pi Pico
 
-typedef struct{
-    uint16_t samples[SAMPLE_COUNT];
-    volatile float dB;
+/**
+ * @brief Structure to hold microphone data.
+ *
+ * This structure stores raw samples from the microphone and the calculated
+ * decibel (dB) value.
+ */
+typedef struct {
+    uint16_t samples[SAMPLE_COUNT];  ///< Array to store raw microphone samples
+    volatile float dB;               ///< Calculated decibel level from the samples
 } micdata_t;
 
+// External declaration of the microphone data instance
 extern micdata_t micdata;
 
+/**
+ * @brief Initializes the microphone.
+ *
+ * This function configures the necessary hardware and settings to start
+ * collecting data from the microphone.
+ */
 void setup_mic();
+
+/**
+ * @brief Collects samples from the microphone.
+ *
+ * This function reads SAMPLE_COUNT values from the microphone and stores
+ * them in the micdata structure for further processing.
+ */
 void collect_samples();
+
+/**
+ * @brief Converts raw microphone samples to decibel (dB) values.
+ *
+ * @param micdata Pointer to the micdata_t structure containing the samples.
+ * @param n_samples Number of samples to process for dB calculation.
+ */
 void digital_to_dB(micdata_t *micdata, uint8_t n_samples);
 
 #endif
